e1dfb957c1e4c32b477480e4ca7193974c0e1e75
el-toxic-053 <r.terweele@live.nl>
2024-03-03 15:04:45 +0100

    Initial commit

---
+++ src/main/java/backendrestaurant/com/example/backendrestaurant/BackendrestaurantApplication.java
@@ -0,0 +1,13 @@
+package backendrestaurant.com.example.backendrestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendrestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendrestaurantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/backendrestaurant/com/example/backendrestaurant/BackendrestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package backendrestaurant.com.example.backendrestaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendrestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
