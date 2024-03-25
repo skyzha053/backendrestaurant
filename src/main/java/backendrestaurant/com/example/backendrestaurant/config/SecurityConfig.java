1aca58937d204053557c58bddbc7729d221887f0
el-toxic-053 <r.terweele@live.nl>
2024-03-25 17:08:24 +0100

    security aangemaakt

---
+++ src/main/java/backendrestaurant/com/example/backendrestaurant/config/SecurityConfig.java
@@ -0,0 +1,75 @@
+package backendrestaurant.com.example.backendrestaurant.config;
+
+import backendrestaurant.com.example.backendrestaurant.security.JwtRequestFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http, JwtRequestFilter jwtRequestFilter) throws Exception {
+        http
+                .httpBasic(hp -> hp.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html"
+                        ).permitAll()
+                        .requestMatchers("/login").permitAll()
+                        .requestMatchers("/omzet/").authenticated()
+                        .requestMatchers("/reservering/").authenticated()
+                        .requestMatchers("/drinks/").authenticated()
+                        .requestMatchers("/users/").authenticated()
+                        .requestMatchers("/bon/").authenticated()
+                        .requestMatchers("/allergie/").authenticated()
+                        .requestMatchers("/menuItems/all/{id}").authenticated()
+                        .requestMatchers("/menuItems/all/{id}").authenticated()
+                        .requestMatchers("/menuItems/all/").authenticated()
+                        .requestMatchers("/menuItems/all/").authenticated()
+                        .requestMatchers("/menuItems/all/{id}").hasRole("CHEFKOK")
+                        .requestMatchers("/menuItems/all/create").hasRole("CHEFKOK")
+                        .requestMatchers("/menuItems/all/{id}/block").hasRole("CHEFKOK")
+                        .requestMatchers("/menuItems/all/{id}/unblock").hasRole("CHEFKOK")
+                        .requestMatchers("/menuItems/**").hasAnyRole("SERVEERSTERS", "RESTAURANTMANAGER")
+                        .requestMatchers("/omzet/**").hasRole("BAAS")
+                        .requestMatchers("/reservering/**").hasAnyRole("BAAS", "RESTAURANTMANAGER")
+                        .requestMatchers("/drinks/dranken/{id}").hasRole("BAAS")
+                        .requestMatchers("/drinks/dranken/plaatsen").hasRole("BAAS")
+                        .requestMatchers("/drinks/dranken/").hasAnyRole("SERVEERSTERS", "RESTAURANTMANAGER")
+                        .requestMatchers("/user/**").hasRole("BAAS")
+                        .requestMatchers("/bon/**").hasAnyRole("BAAS", "RESTAURANTMANAGER", "SERVEERSTER")
+                        .requestMatchers("/allergie/**").hasAnyRole("BAAS", "RESTAURANTMANAGER", "SERVEERSTER")
+                        .anyRequest().denyAll()
+                )
+                .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
+                .csrf(csrf -> csrf.disable())
+                .cors(cors -> {})
+                .sessionManagement( session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        ;
+        return  http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authManager(HttpSecurity http, PasswordEncoder encoder, UserDetailsService apiUserDetailsService) throws Exception {
+        var builder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        builder.userDetailsService(apiUserDetailsService).passwordEncoder(encoder);
+        return builder.build();
+    }
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,86 @@
+-- Voeg allergieën toe
+INSERT INTO public.allergie (id, naam)
+VALUES
+    (1, 'gluten'),
+    (2, 'noten'),
+    (3, 'ei'),
+    (4, 'lupine'),
+    (5, 'melk'),
+    (6, 'sesamzaad'),
+    (7, 'zwaveldioxide'),
+    (8, 'selderij'),
+    (9, 'pindas'),
+    (10, 'mosterd'),
+    (11, 'vis'),
+    (12, 'weekdieren'),
+    (13, 'soja'),
+    (14, 'schaaldieren');
+
+-- Voeg menu-items toe
+INSERT INTO public.menu_item (id, available, description, name, price)
+VALUES
+    (1, true, 'Knoflooksaus Kruidenboter Olijftapenade', 'Mediterraans Landbrood', 7.00),
+    (2, true, 'Pommodori Knoflook Basilicum Olijfolie', 'Bruschetta Pommodori', 10.00),
+    (3, true, 'Pommodori Tallegio Carpaccio Zalm Parmaham', 'Bruschetta Classico', 10.00);
+
+-- Voeg koppelingen tussen menu-items en allergieën toe
+INSERT INTO public.menu_item_allergenen (menu_items_id, allergenen_id)
+VALUES
+    (1, 1),
+    (1, 2),
+    (2, 1),
+    (2, 5),
+    (3, 11);
+-- Voeg dranken toe aan de tabel 'drank'
+INSERT INTO public.drank (id, naam, prijs, type)
+VALUES
+    (1, 'Cola', 2.50, 'Frisdrank'),
+    (2, 'Fanta', 2.50, 'Frisdrank');
+
+
+INSERT INTO public.reservation (id, allergies, bedrijfsnaam, comments, email_address, name, number_of_persons, phone_number, reservation_date)
+VALUES
+    (1, 'geen', NULL, 'Graag bij het raam', 'r.terweele@live.nl', 'Remon ter Weele', 9, '0638998771', '2024-05-05');
+
+INSERT INTO public.tafel (paid, totale_prijs, id, naam)
+VALUES
+    ('f', 28.50, 1, 'Ayla');
+
+INSERT INTO public.besteld_item (hoeveelheid, prijs, bon_id, drank_id, id, menu_item_id, tafel_id, item_naam)
+VALUES
+    (3, 21.00, NULL, NULL, 1, 1, 1, 'Mediterraans Landbrood'),
+    (1, 2.50, NULL, 1, 2, NULL, 1, 'Cola'),
+    (2, 5.00, NULL, 2, 3, NULL, 1, 'Fanta');
+
+
+-- Voeg gegevens toe aan de tabel 'omzet'
+INSERT INTO public.omzet (id, datum, omzet_bedrag)
+VALUES
+    (1, '2024-03-13 18:57:09.054744', 867),
+    (2, '2024-03-13 18:59:12.589553', 964);
+
+
+
+INSERT INTO public.roles (active, id, description, role_name)
+VALUES
+    (true, 2, 'Gebruikersrollen', 'ROLE_USER'),
+    (true, 3, 'Chef-kok', 'ROLE_CHEFKOK'),
+    (true, 1, 'Administrator rollen', 'ROLE_BAAS'),
+    (true, 4, 'Serveerster', 'ROLE_SERVEERSTERS'),
+    (true, 5, 'Restaurantmanager', 'ROLE_RESTAURANTMANAGER');
+
+
+
+    INSERT INTO public.users (are_credentials_expired, is_enabled, is_expired, is_locked, id, password, user_name)
+VALUES
+    (false, true, false, false, 8, '$2a$04$CEO.nbew/ulIV8uxlg.FeegqtxgOCXsis47peCxM6d4SRHVngufGa', 'Remon'),
+    (false, true, false, false, 9, '$2a$04$GSZGLOno9slqQ1O024d8WeZKIZgOwHdvhaAdCJ3daV3B9e3dzxzDy', 'Yente'),
+    (false, true, false, false, 10, '$2a$04$GSZGLOno9slqQ1O024d8WeZKIZgOwHdvhaAdCJ3daV3B9e3dzxzDy', 'Emma'),
+    (false, true, false, false, 11, '$2a$04$tsAvQUlJ56C4E0bk4MTYwumQCgZCbg3/jR1JwVoxtrK.bAegkttYe', 'Chikit');
+
+INSERT INTO public.user_role (user_id, role_id)
+VALUES
+    (8, 1),
+    (9, 5),
+    (11, 3),
+    (10, 4);
