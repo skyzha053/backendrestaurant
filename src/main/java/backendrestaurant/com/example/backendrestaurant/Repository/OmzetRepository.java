7606fd2d7537a3a5d81a1729110f54d9c828e568
el-toxic-053 <r.terweele@live.nl>
2024-03-26 10:58:02 +0100

    omzet eruit gehaald

--- src/main/java/backendrestaurant/com/example/backendrestaurant/Repository/OmzetRepository.java
+++
@@ -1,6 +0,0 @@
-package backendrestaurant.com.example.backendrestaurant.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import backendrestaurant.com.example.backendrestaurant.Entiteit.Omzet;
-public interface OmzetRepository extends JpaRepository<Omzet, Long> {
-    // Add custom queries if needed
-}
