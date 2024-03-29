f3c476f06f2a659852e49d6483badc051b452bd7
el-toxic-053 <r.terweele@live.nl>
2024-03-29 14:05:58 +0100

    id weg gehaald

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -17,11 +17,10 @@ VALUES
     (14, 'schaaldieren');
 
 -- Voeg menu-items toe
-INSERT INTO public.menu_item (id, available, description, name, price)
-VALUES
-    (1, true, 'Knoflooksaus Kruidenboter Olijftapenade', 'Mediterraans Landbrood', 7.00),
-    (2, true, 'Pommodori Knoflook Basilicum Olijfolie', 'Bruschetta Pommodori', 10.00),
-    (3, true, 'Pommodori Tallegio Carpaccio Zalm Parmaham', 'Bruschetta Classico', 10.00);
+INSERT INTO public.menu_item (available, description, name, price) VALUES
+(true, 'Knoflooksaus Kruidenboter Olijftapenade', 'Mediterraans Landbrood', 7.00),
+(true, 'Pommodori Knoflook Basilicum Olijfolie', 'Bruschetta Pommodori', 10.00),
+(true, 'Pommodori Tallegio Carpaccio Zalm Parmaham', 'Bruschetta Classico', 10.00);
 
 -- Voeg koppelingen tussen menu-items en allergieën toe
 INSERT INTO public.menu_item_allergenen (menu_items_id, allergenen_id)
@@ -32,37 +31,37 @@ VALUES
     (2, 5),
     (3, 11);
 -- Voeg dranken toe aan de tabel 'drank'
-INSERT INTO public.drank (id, naam, prijs, type)
+INSERT INTO public.drank ( naam, prijs, type)
 VALUES
-    (1, 'Cola', 2.50, 'Frisdrank'),
-    (2, 'Fanta', 2.50, 'Frisdrank');
+    ( 'Cola', 2.50, 'Frisdrank'),
+    ( 'Fanta', 2.50, 'Frisdrank');
 
 
-INSERT INTO public.reservation (id, allergies, bedrijfsnaam, comments, email_address, name, number_of_persons, phone_number, reservation_date)
+INSERT INTO public.reservation ( allergies, bedrijfsnaam, comments, email_address, name, number_of_persons, phone_number, reservation_date)
 VALUES
-    (1, 'geen', NULL, 'Graag bij het raam', 'r.terweele@live.nl', 'Remon ter Weele', 9, '0638998771', '2024-05-05');
+    ( 'geen', NULL, 'Graag bij het raam', 'r.terweele@live.nl', 'Remon ter Weele', 9, '0638998771', '2024-05-05');
 
 
-INSERT INTO public.roles (active, id, description, role_name)
+INSERT INTO public.roles (active, description, role_name)
 VALUES
-    (true, 2, 'Gebruikersrollen', 'ROLE_USER'),
-    (true, 3, 'Chef-kok', 'ROLE_CHEFKOK'),
-    (true, 1, 'Administrator rollen', 'ROLE_BAAS'),
-    (true, 4, 'Serveerster', 'ROLE_SERVEERSTERS'),
-    (true, 5, 'Restaurantmanager', 'ROLE_RESTAURANTMANAGER');
+    (true,  'Administrator rollen', 'ROLE_BAAS'),
+    (true,  'Restaurantmanager', 'ROLE_RESTAURANTMANAGER'),
+    (true,  'Serveerster', 'ROLE_SERVEERSTERS'),
+    (true,  'Chef-kok', 'ROLE_CHEFKOK');
 
 
 
-    INSERT INTO public.users (are_credentials_expired, is_enabled, is_expired, is_locked, id, password, user_name)
+
+    INSERT INTO public.users (are_credentials_expired, is_enabled, is_expired, is_locked,  password, user_name)
 VALUES
-    (false, true, false, false, 8, '$2a$04$CEO.nbew/ulIV8uxlg.FeegqtxgOCXsis47peCxM6d4SRHVngufGa', 'Remon'),
-    (false, true, false, false, 9, '$2a$04$GSZGLOno9slqQ1O024d8WeZKIZgOwHdvhaAdCJ3daV3B9e3dzxzDy', 'Yente'),
-    (false, true, false, false, 10, '$2a$04$GSZGLOno9slqQ1O024d8WeZKIZgOwHdvhaAdCJ3daV3B9e3dzxzDy', 'Emma'),
-    (false, true, false, false, 11, '$2a$04$tsAvQUlJ56C4E0bk4MTYwumQCgZCbg3/jR1JwVoxtrK.bAegkttYe', 'Chikit');
+    (false, true, false, false,  '$2a$04$CEO.nbew/ulIV8uxlg.FeegqtxgOCXsis47peCxM6d4SRHVngufGa', 'Remon'),
+    (false, true, false, false,  '$2a$04$GSZGLOno9slqQ1O024d8WeZKIZgOwHdvhaAdCJ3daV3B9e3dzxzDy', 'Yente'),
+    (false, true, false, false,  '$2a$04$GSZGLOno9slqQ1O024d8WeZKIZgOwHdvhaAdCJ3daV3B9e3dzxzDy', 'Emma'),
+    (false, true, false, false,  '$2a$04$tsAvQUlJ56C4E0bk4MTYwumQCgZCbg3/jR1JwVoxtrK.bAegkttYe', 'Chikit');
 
 INSERT INTO public.user_role (user_id, role_id)
 VALUES
-    (8, 1),
-    (9, 5),
-    (11, 3),
-    (10, 4);
+    (1, 1),
+    (2, 2),
+    (3, 3),
+    (4, 4);
