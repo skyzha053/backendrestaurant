80dab42591d43c2fe1d1d2a6ea2ba07ba220b0d9
el-toxic-053 <r.terweele@live.nl>
2024-04-22 08:49:28 +0200

    response aanngepast en een putmapping toegevoegd

--- src/main/java/backendrestaurant/com/example/backendrestaurant/Controller/AllergieController.java
+++ src/main/java/backendrestaurant/com/example/backendrestaurant/Controller/AllergieController.java
@@ -4,30 +4,50 @@ import backendrestaurant.com.example.backendrestaurant.Entiteit.Allergie;
 import backendrestaurant.com.example.backendrestaurant.Service.AllergieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/allergieen")
+@RequestMapping("/allergies")
 public class AllergieController {
 
     @Autowired
     private AllergieService allergieService;
 
     @GetMapping
-    public List<Allergie> getAllAllergieen() {
-        return allergieService.getAllAllergieen();
+    public ResponseEntity<List<Allergie>> getAllergies() {
+        List<Allergie> allergies = allergieService.getAllAllergieen();
+        return ResponseEntity.ok().body(allergies);
     }
 
     @GetMapping("/{id}")
-    public Allergie getAllergieById(@PathVariable Long id) {
-        return allergieService.getAllergieById(id);
+    public ResponseEntity<Allergie> getAllergieById(@PathVariable Long id) {
+        Allergie allergie = allergieService.getAllergieById(id);
+        if (allergie != null) {
+            return ResponseEntity.ok().body(allergie);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @PostMapping
-    public Allergie createAllergie(@RequestBody Allergie allergie) {
-        return allergieService.createAllergie(allergie);
+    public ResponseEntity<Allergie> createAllergie(@RequestBody Allergie allergie) {
+        Allergie createdAllergie = allergieService.createAllergie(allergie);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdAllergie);
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Allergie> updateAllergie(@PathVariable Long id, @RequestBody Allergie allergieDetails) {
+        Allergie updatedAllergie = allergieService.updateAllergie(id, allergieDetails);
+        if (updatedAllergie != null) {
+            return ResponseEntity.ok().body(updatedAllergie);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
 
 
 }
